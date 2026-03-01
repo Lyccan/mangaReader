926fe4e849c88baff76292f8045664fbade75165
Lyccan <topchernobyl@gmail.com>
2026-03-01 12:08:25 -0300

    feat(api): adiciona serviço de criação de usuário.

---
+++ mangaReader/Services/UserService.cs
@@ -0,0 +1,26 @@
+﻿using mangaReader.Data;
+using mangaReader.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mangaReader.Services
+{
+    public class UserService
+    {
+        private readonly IMongoCollection<User> _users;
+
+        public UserService(MongoDbContext context)
+        {
+            _users = context.GetCollection<User>("users");
+        }
+
+        public async System.Threading.Tasks.Task CreateUser(User user)
+        {
+            _users.InsertOneAsync(user);
+        }
+
+
+    }
+}
